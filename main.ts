6c25c28c12ec3cc5e5fa17a5c4675b6f8488424f
ande209x <65941409+ande209x@users.noreply.github.com>
2020-05-26 08:39:46 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,109 @@
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . # . #
+            # . . . #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("johanes er den bedste lære")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("jeg ælsker skole")
+})
+let lys = 0
+let pligt = 0
+pligt += 0
+basic.forever(function () {
+    while (pligt < 1023) {
+        pins.analogWritePin(AnalogPin.P0, pligt)
+        pligt = pligt + 1
+        basic.pause(10)
+    }
+    while (pligt > 0) {
+        pins.analogWritePin(AnalogPin.P0, pligt)
+        pligt = pligt + 1
+        basic.pause(10)
+    }
+})
+basic.forever(function () {
+    lys = pins.analogReadPin(AnalogPin.P0)
+    if (lys > 200) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            . # # # .
+            . . # # .
+            . # # # .
+            # # # . .
+            `)
+    }
+})
